344ea916dbff907d1e59574a876af2c60e1a53ea
tut233322 <153588162+tut233322@users.noreply.github.com>
2024-01-08 17:32:42 +0900

    delete debug comment, add printStackTrace
    now I understand what happened.
    
    If we don't initialize Frequency properly, NullPointerException will be thrown.
    I think I do NOT have to fix Frequency for now, so leave it.

--- s4/B233322/TestCase.java
+++ s4/B233322/TestCase.java
@@ -44,7 +44,6 @@ public class TestCase {
 	    myObject.setTarget("H".getBytes());
 	    freq = myObject.frequency();
 	    assert freq == 4: "Hi Ho Hi Ho, H: " + freq;
-        System.out.println("ok");
         
 	    // Write your testCase here
         // Test case 1: target is not set
@@ -52,7 +51,6 @@ public class TestCase {
         myObject.setSpace("Hi Ho Hi Ho".getBytes());
         freq = myObject.frequency();
         assert freq == -1 : "test failed: when target not set, return value must be -1. but return value is " + freq + ".";
-        System.out.println("ok");
         
         // Test case 2: target length is zero
         myObject = new Frequencer();
@@ -60,14 +58,12 @@ public class TestCase {
         myObject.setTarget("".getBytes());
         freq = myObject.frequency();
         assert freq == -1 : "test failed: when target length is zero, return value must be -1. but return value is " + freq + ".";
-        System.out.println("ok");
         
         // Test case 3: space is not set
         myObject = new Frequencer();
         myObject.setTarget("H".getBytes());
         freq = myObject.frequency();
         assert freq == 0 : "test failed: when space is not set, return value must be 0. but return value is " + freq + ".";
-        System.out.println("ok");
         
         // Test case 4: space length is zero
         myObject = new Frequencer();
@@ -75,7 +71,6 @@ public class TestCase {
         myObject.setTarget("H".getBytes());
         freq = myObject.frequency();
         assert freq == 0 : "test failed: when space length is zero, return value must be 0. but return value is " + freq + ".";
-        System.out.println("ok");
         
         // Test case 5: valid case
         myObject = new Frequencer();
@@ -83,7 +78,6 @@ public class TestCase {
         myObject.setTarget("H".getBytes());
         freq = myObject.frequency();
         assert freq == 4 : "test failed: space is \"Hi Ho Hi Ho\", target is \"H\", return value must be 4, but it is " + freq + ".";
-        System.out.println("ok");
         
         // Test case 6: valid case
         myObject = new Frequencer();
@@ -91,7 +85,6 @@ public class TestCase {
         myObject.setTarget("X".getBytes());
         freq = myObject.frequency();
         assert freq == 0 : "test failed: space is \"Hi Ho Hi Ho\", target is \"X\", return value must be 0, but it is " + freq + ".";
-        System.out.println("ok");
         
 
         //detect the problem(week2 - step6)
@@ -108,6 +101,7 @@ public class TestCase {
         catch (Exception e)
         {
             System.out.println("test case 7: problem remain!");
+        e.printStackTrace();
         }
 	}
 	catch(Exception e) {
@@ -137,6 +131,7 @@ public class TestCase {
 	}
 	catch(Exception e) {
 	    System.out.println("Exception occurred in InformationEstimator Object");
+        e.printStackTrace();
 	    success = false;
 	}
         if(success) { System.out.println("TestCase OK"); } 
